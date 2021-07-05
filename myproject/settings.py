8e7bd3e7ec776850b8e173a8efd18aca18686644
ishwar <80237206+PaulSanjoy@users.noreply.github.com>
2021-07-05 16:30:07 +0530

    change in web

--- myproject/settings.py
+++ myproject/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = True
 
 ALLOWED_HOSTS = ['*'] 
 
@@ -206,3 +206,4 @@ SOCIALACCOUNT_PROVIDERS = {
         ],
     }
 }
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
